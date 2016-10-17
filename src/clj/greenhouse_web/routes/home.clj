0ccaac0415714bdb1162bf88a2966e3ab011f3a8
jonson <jdeyoung@gmail.com>
2016-10-16 22:14:52 -0700

    Add sensor mapping

--- src/clj/greenhouse_web/routes/home.clj
+++ src/clj/greenhouse_web/routes/home.clj
@@ -7,6 +7,7 @@
 
 ;todo: figure out where config should live!
 (def url "http://nas.oscar:8086/query")
+(def sensor-mapping (hash-map :basil "a2" :thyme "a0" :rosemary "a1"))
 (defn moisture-influx-query [sensor] (format "SELECT last(value) FROM moisture where sensor = '%s'" sensor))
 (defn latest-influx-value [sensor] (client/get url {:query-params {"db" "sensors" "q" (moisture-influx-query sensor)} :as :json})) 
 (defn latest-value [sensor] (get 
@@ -18,14 +19,14 @@
 
 (defn home-page []
   (layout/render
-    "home.html" {:basil (latest-value "a0")
-                 :thyme (latest-value "a1")
-                 :rosemary (latest-value "a2")}))                
+    "home.html" {:basil (latest-value (get sensor-mapping :basil))
+                 :thyme (latest-value (get sensor-mapping :thyme))
+                 :rosemary (latest-value (get sensor-mapping :rosemary))}))                
 
 (defn about-page []
   (layout/render "about.html"))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
+  (GET "/" [] (home-page)))
+
 
